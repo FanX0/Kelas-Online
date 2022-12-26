b724dd9ee772c77f67c66293d36a3f7dcfa209d6
Sumptu <narendraajus27@gmail.com>
2022-12-27 02:18:53 +0800

    pepekkeke

--- service-user/seeders/20221222130538-user-seeders.js
+++ service-user/seeders/20221222130538-user-seeders.js
@@ -17,7 +17,13 @@ await queryInterface.bulkInsert('users', [
 <<<<<<< HEAD
      },
      {
-      
+      name: "naren",
+      profession: "Front End Developer",
+      role: "student",
+      email: "naren123@gmail.com",
+      password: await bcrypt.hash('naren123',10),
+      created_at: new Date(),
+      updated_at: new Date()
 
     },
 =======
