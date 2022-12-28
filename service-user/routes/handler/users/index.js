4c5f0864e377fe2c8150af94c59556a9f41a9cdb
FanX0 <faridazhari46@gmail.com>
2022-12-29 00:06:06 +0700

    update api user

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -1,6 +1,8 @@
 const register = require('./register');
 const login = require ('./login')
+const update = require ('./update')
 module.exports = {
     register,
-    login
+    login,
+    update
 }
