5031cec97f6a7ff5f664cd66542605c92ba1e483
FanX0 <faridazhari46@gmail.com>
2022-12-28 23:35:17 +0700

    create api login

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -1,5 +1,6 @@
 const register = require('./register');
-
+const login = require ('./login')
 module.exports = {
-    register
+    register,
+    login
 }
--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const usersHandler = require('./handler/users');
 
-router.post('/register', usersHandler.register);
+router.post('/register', usersHandler.register)
+router.post('/login', usersHandler.login)
 
 module.exports = router;
