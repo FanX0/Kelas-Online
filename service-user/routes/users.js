4ad47b9716c82cefb9bd51a155d0c06a54d12914
Sumptu <narendraajus27@gmail.com>
2022-12-30 00:23:14 +0800

    API logout

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const usersHandler = require('./handler/users');
 
-router.post('/register', usersHandler.register)
-router.post('/login', usersHandler.login)
-router.put('/:id', usersHandler.update)
-router.get('/:id', usersHandler.getUser)
-router.get('/', usersHandler.getUsers)
+router.post('/register', usersHandler.register);
+router.post('/login', usersHandler.login);
+router.post('/logout', usersHandler.logout);
+router.put('/:id', usersHandler.update);
+router.get('/:id', usersHandler.getUser);
+router.get('/', usersHandler.getUsers);
+
 module.exports = router;
