88c1118b5b6c3739c94414e13ec0ac2465042e48
FanX0 <faridazhari46@gmail.com>
2022-12-31 22:56:31 +0700

    Integrasi ApI Register

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var router = express.Router();
-const { APP_NAME } = process.env;
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(users);
-});
+const usersHandler = require ('./handler/users');
+
+
+router.post('/register',usersHandler.register );
 
 module.exports = router;
