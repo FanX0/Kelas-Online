3624233ef152d60c348140a971d1526d84f3a685
Sumptu <narendraajus27@gmail.com>
2023-01-11 18:38:24 +0800

    issue error courses

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,30 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const coursesRouter = require ('./routes/courses');
-const mediaRouter = require ('./routes/media');
-const orderRouter = require ('./routes/order');
-const paymentsRouter = require ('./routes/payments');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const coursesRouter = require("./routes/courses");
+const mediaRouter = require("./routes/media");
+const orderRouter = require("./routes/order");
+const paymentsRouter = require("./routes/payments");
 
+const verifyToken = require("./middlewares/verifyToken");
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/courses', coursesRouter);
-app.use('/media',mediaRouter);
-app.use('/orders',orderRouter);
-app.use('/payments', paymentsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/courses", verifyToken, coursesRouter);
+app.use("/media", mediaRouter);
+app.use("/orders", orderRouter);
+app.use("/payments", paymentsRouter);
 
 module.exports = app;
